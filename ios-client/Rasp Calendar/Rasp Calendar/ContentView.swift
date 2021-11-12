5aaba3ee34bae515ef79563c35b59d3c39ad54cb
Noel Gomez <noelgomez@Noels-MacBook-Pro.local>
2021-11-11 23:34:47 -0800

    ios init

---
+++ ios-client/Rasp Calendar/Rasp Calendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rasp Calendar
+//
+//  Created by Noel Gomez on 11/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("lols")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-client/Rasp Calendar/Rasp Calendar/Rasp_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rasp_CalendarApp.swift
+//  Rasp Calendar
+//
+//  Created by Noel Gomez on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Rasp_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
